b7b18f0576ae99001c74e1adc6f1d87554409586
“Akshola00” <akinniyishola07@gmail.com>
2024-11-14 16:07:07 +0100

    chore

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import MarqueeContent from "marquee-content";
+// import MarqueeContent from "marquee-content";
 
 const Marquee = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  MarqueeContent.registerGSAP(gsap, ScrollTrigger);
+//   gsap.registerPlugin(ScrollTrigger);
+//   MarqueeContent.registerGSAP(gsap, ScrollTrigger);
 
-  const marquee = new MarqueeContent({
-    element: ".marquee",
-  });
+//   const marquee = new MarqueeContent({
+//     element: ".marquee",
+//   });
 
-  marquee.init();
+//   marquee.init();
 
   return (
     <div>
