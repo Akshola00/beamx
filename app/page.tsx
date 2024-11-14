1789d081e9b2ae5a2899633dde30ce9ebf0c4ce0
“Akshola00” <akinniyishola07@gmail.com>
2024-11-14 15:57:55 +0100

    chore

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import ParticleGround from "particleground.ts";
 import { AnimatedBackground } from "animated-backgrounds";
 
 export default function Home() {
