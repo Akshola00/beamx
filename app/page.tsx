dd785bfb28047de4b09b165b8170f12740091e4c
“Akshola00” <akinniyishola07@gmail.com>
2024-11-14 16:02:31 +0100

    chore

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import { AnimatedBackground } from "animated-backgrounds";
+
 
 export default function Home() {
   return (
     <div>
-      <AnimatedBackground animationName="starryNight" />
+      
       <Header />
       <Hero />
     </div>
